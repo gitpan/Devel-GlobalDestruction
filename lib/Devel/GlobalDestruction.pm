84d73f599a0020af39c97f6888d0fd37691caffe
Peter Rabbitson <ribasushi@cpan.org>
2012-08-08 23:07:15 +0000

    Import of RIBASUSHI/Devel-GlobalDestruction-0.09 from CPAN.
    gitpan-cpan-distribution: Devel-GlobalDestruction
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         RIBASUSHI/Devel-GlobalDestruction-0.09.tar.gz
    gitpan-cpan-author:       RIBASUSHI
    gitpan-cpan-maturity:     released

--- lib/Devel/GlobalDestruction.pm
+++ lib/Devel/GlobalDestruction.pm
@@ -3,68 +3,99 @@ package Devel::GlobalDestruction;
 use strict;
 use warnings;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use Sub::Exporter::Progressive -setup => {
-    exports => [ qw(in_global_destruction) ],
-    groups  => { default => [ -all ] },
+  exports => [ qw(in_global_destruction) ],
+  groups  => { default => [ -all ] },
 };
 
 # we run 5.14+ - everything is in core
 #
 if (defined ${^GLOBAL_PHASE}) {
-    eval 'sub in_global_destruction () { ${^GLOBAL_PHASE} eq q[DESTRUCT] }';
+  eval 'sub in_global_destruction () { ${^GLOBAL_PHASE} eq q[DESTRUCT] }; 1'
+    or die $@;
 }
 # try to load the xs version if it was compiled
 #
 elsif (eval {
-    require XSLoader;
-    XSLoader::load(__PACKAGE__, $VERSION);
-    1;
+  require XSLoader;
+  XSLoader::load(__PACKAGE__, $VERSION);
+  1;
 }) {
-    # the eval already installed everything, nothing to do
+  # the eval already installed everything, nothing to do
 }
 # Not core nor XS
+# The whole thing is in an eval to prevent perl from parsing it in the
+# first place under perls where none of this is needed
 #
 else {
-
-  # SpeedyCGI runs END blocks every cycle but somehow keeps object instances
-  # hence DIAF
-  die("The pure-perl version of @{[__PACKAGE__]} can not function correctly under CGI::SpeedyCGI. "
-    . "Please ensure you have a working compiler, and reinstall @{[__PACKAGE__]} to enable the XS "
-    . "codepath.\n"
-  ) if $CGI::SpeedyCGI::i_am_speedy;
-
   eval <<'PP_IGD' or die $@;
+
+# SpeedyCGI runs END blocks every cycle but somehow keeps object instances
+# hence DIAF
+die("The pure-perl version of @{[__PACKAGE__]} can not function correctly under CGI::SpeedyCGI. "
+  . "Please ensure you have a working compiler, and reinstall @{[__PACKAGE__]} to enable the XS "
+  . "codepath.\n"
+) if $CGI::SpeedyCGI::i_am_speedy;
 
 my ($in_global_destruction, $before_is_installed);
 
 sub in_global_destruction () { $in_global_destruction }
 
+# end_av trick suggested by liz++
+require B;
+my $add_endblock = sub {
+  push @{ B::end_av()->object_2svref }, sub { $in_global_destruction = 1 };
+};
+
 # This block will fire towards the end of the program execution
-# Since there is no way for us to generate an END which will execute *last*
-# this is *NOT 100% INCOMPATIBLE* with XS/${^GLOBAL_PHASE}. We *may* end up
-# with a true in_gloal_destruction() in the middle of another END block
-# There are no practical cases where this matters.
+# Use it to inject an END block which is guaranteed to run last
+# (as long as something else doesn't inject yet another block in
+# the same manner afterwards, at which point it hardly matters
+# anyway)
 #
-END {
-  $in_global_destruction = 1;
-}
+END { $add_endblock->() }
 
 # threads do not execute the global ENDs (it would be stupid). However
-# one can register a new END via simple string eval within a thread, and
+# one can register a new thread-local END from within a thread, and
 # achieve the same result. A logical place to do this would be CLONE, which
 # is claimed to run in the context of the new thread. However this does
-# not really seem to be the case - any END evaled in a CLONE is ignored :(
+# not really seem to be the case - any END inserted in a CLONE is ignored :(
 # Hence blatantly hooking threads::create
 #
 if ($INC{'threads.pm'}) {
+  require Scalar::Util;
+
   my $orig_create = threads->can('create');
   no warnings 'redefine';
+
   *threads::create = sub {
-    { local $@; eval 'END { $in_global_destruction = 1 }' };
+    my $class = shift;
+    my $target = shift;
+
+    unless ( (Scalar::Util::reftype($target)||'') eq 'CODE' ) {
+      no strict 'refs';
+      $target = \&{ caller() . "::$target" };
+    }
+
+    @_ = (
+      $class,
+      sub {
+        # Perls compiled with THREADS_HAVE_PIDS do not copy end_av properly
+        # between threads, so B::end_av ends up returning a B::SPECIAL and it
+        # goes downhill from there
+        # Install a noop END just to be on the safe side
+        { local $@; eval 'END {}' }
+        $add_endblock->();
+        goto $target
+      },
+      @_,
+    );
+
     goto $orig_create;
   };
+
   $before_is_installed = 1;
 }
 
@@ -143,6 +174,8 @@ Jesse Luehrs E<lt>doy@tozt.netE<gt>
 Peter Rabbitson E<lt>ribasushi@cpan.orgE<gt>
 
 Arthur Axel 'fREW' Schmidt E<lt>frioux@gmail.comE<gt>
+
+Elizabeth Mattijsen E<lt>liz@dijkmat.nlE<gt>
 
 =head1 COPYRIGHT
 
--- t/01_basic.t
+++ t/01_basic.t
@@ -2,32 +2,64 @@ use strict;
 use warnings;
 
 BEGIN {
-    if ($ENV{DEVEL_GLOBALDESTRUCTION_PP_TEST}) {
-        require DynaLoader;
-        no warnings 'redefine';
-        my $orig = \&DynaLoader::bootstrap;
-        *DynaLoader::bootstrap = sub {
-            die 'no XS' if $_[0] eq 'Devel::GlobalDestruction';
-            goto $orig;
-        };
+  if ($ENV{DEVEL_GLOBALDESTRUCTION_PP_TEST}) {
+    no strict 'refs';
+    no warnings 'redefine';
+
+    for my $f (qw(DynaLoader::bootstrap XSLoader::load)) {
+      my ($mod) = $f =~ /^ (.+) \:\: [^:]+ $/x;
+      eval "require $mod" or die $@;
+
+      my $orig = \&$f;
+      *$f = sub {
+        die 'no XS' if ($_[0]||'') eq 'Devel::GlobalDestruction';
+        goto $orig;
+      };
     }
+  }
 }
 
 BEGIN {
-    package Test::Scope::Guard;
-    sub new { my ($class, $code) = @_; bless [$code], $class; }
-    sub DESTROY { my $self = shift; $self->[0]->() }
+  package Test::Scope::Guard;
+  sub new { my ($class, $code) = @_; bless [$code], $class; }
+  sub DESTROY { my $self = shift; $self->[0]->() }
 }
 
-print "1..6\n";
+print "1..9\n";
 
-my $had_error = 0;
-END { $? = $had_error };
+our $had_error;
+
+# try to ensure this is the last-most END so we capture future tests
+# running in other ENDs
+require B;
+my $reinject_retries = my $max_retry = 5;
+my $end_worker;
+$end_worker = sub {
+  my $tail = (B::end_av()->ARRAY)[-1];
+  if (!defined $tail or $tail == $end_worker) {
+    $? = $had_error || 0;
+    $reinject_retries = 0;
+  }
+  elsif ($reinject_retries--) {
+    push @{B::end_av()->object_2svref}, $end_worker;
+  }
+  else {
+    print STDERR "\n\nSomething is racing with @{[__FILE__]} for final END block definition - can't win after $max_retry iterations :(\n\n";
+    require POSIX;
+    POSIX::_exit( 255 );
+  }
+};
+END { push @{B::end_av()->object_2svref}, $end_worker }
+
 sub ok ($$) {
-    $had_error++, print "not " if !$_[0];
-    print "ok";
-    print " - $_[1]" if defined $_[1];
-    print "\n";
+  $had_error++, print "not " if !$_[0];
+  print "ok";
+  print " - $_[1]" if defined $_[1];
+  print "\n";
+}
+
+END {
+  ok( ! in_global_destruction(), 'Not yet in GD while in END block 2' )
 }
 
 ok( eval "use Devel::GlobalDestruction; 1", "use Devel::GlobalDestruction" );
@@ -38,6 +70,13 @@ ok( defined prototype \&in_global_destruction, "defined prototype" );
 
 ok( prototype \&in_global_destruction eq "", "empty prototype" );
 
-ok( !in_global_destruction(), "not in GD" );
+ok( ! in_global_destruction(), "Runtime is not GD" );
 
-our $sg = Test::Scope::Guard->new(sub { ok( in_global_destruction(), "in GD" ) });
+our $sg1 = Test::Scope::Guard->new(sub { ok( in_global_destruction(), "Final cleanup object destruction properly in GD" ) });
+
+END {
+  ok( ! in_global_destruction(), 'Not yet in GD while in END block 1' )
+}
+
+our $sg2 = Test::Scope::Guard->new(sub { ok( ! in_global_destruction(), "Object destruction in END not considered GD" ) });
+END { undef $sg2 }
